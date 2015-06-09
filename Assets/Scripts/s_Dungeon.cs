835b326e26154968e2f4f0a35a30bafd44ea45c4
Breadmouth <ipspdowling@gmail.com>
2015-06-09 15:50:15 +1000

    New Direction for project Rogerlike: turn-based/stategy endless runner. Added code for random spawning of walls.

--- Assets/Scripts/s_Dungeon.cs
+++ Assets/Scripts/s_Dungeon.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class s_Dungeon : MonoBehaviour {
 
@@ -7,42 +8,41 @@ public class s_Dungeon : MonoBehaviour {
 	
 	public TileTypes[,] m_dungeon;
 
+	public float tempTurnSwitch = 1.0f;
+
 	public GameObject m_wall;
 	public GameObject m_player;
 	public GameObject m_enemy;
+
+	private List<GameObject> m_enemies;
+	private List<GameObject> m_walls;
+
+	private int m_maxWallInLine = 1;
+	private int m_maxEnemyInLine = 1;
+
+	private bool m_playerInput = false;
 	
 	// Use this for initialization
 	void Start () {
-		m_dungeon = new TileTypes[100,100];
+		m_dungeon = new TileTypes[9,12];
+
+		m_enemies = new List<GameObject>();
+		m_walls = new List<GameObject>();
 
 		//setup random dungeon
-		for (int i = 0; i < 100; ++i)
+		for (int i = 0; i < 9; ++i)
 		{
-			for (int j = 0; j < 100; ++j)
+			for (int j = 0; j < 12; ++j)
 			{
-				m_dungeon[i, j] = TileTypes.EMPTY;
-			}
-		}
-		m_dungeon[1, 0] = TileTypes.WALL;
-		m_dungeon[0, 0] = TileTypes.PLAYER;
-		m_dungeon[0, 1] = TileTypes.SPIDER;
-
-		//spawn walls where wall belong
-		for (int i = 0; i < 100; ++i)
-		{
-			for (int j = 0; j < 100; ++j)
-			{
-				if (m_dungeon[i, j] == TileTypes.WALL)
+				if (i == 0 || i == 8)
 				{
+					m_dungeon[i, j] = TileTypes.WALL;
 					GameObject newWall = (GameObject)Instantiate (m_wall, new Vector3(i, j, -1),transform.rotation);
+					m_walls.Add(newWall);
 				}
-				if (m_dungeon[i, j] == TileTypes.PLAYER)
+				else
 				{
-					GameObject newPlayer = (GameObject)Instantiate (m_player, new Vector3(i, j, -1),transform.rotation);
-				}
-				if (m_dungeon[i, j] == TileTypes.SPIDER)
-				{
-					GameObject newEnemy = (GameObject)Instantiate (m_enemy, new Vector3(i, j, -1),transform.rotation);
+					m_dungeon[i, j] = TileTypes.EMPTY;
 				}
 			}
 		}
@@ -50,6 +50,62 @@ public class s_Dungeon : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		tempTurnSwitch -= Time.deltaTime;
+
+		if (!m_playerInput)
+		{
+			if (tempTurnSwitch < 0)
+			{
+				m_playerInput = true;
+				tempTurnSwitch = 1.0f;
+			}
+			//wait for the player to make an action
+		}
+		else
+		{
+			//push everything down
+			for (int i = 0; i < m_walls.Count; ++i)
+			{
+				m_walls[i].transform.position = new Vector3(m_walls[i].transform.position.x,
+				                                            m_walls[i].transform.position.y - 1,
+				                                            m_walls[i].transform.position.z);
+			}
+			for (int i = 0; i < m_enemies.Count; ++i)
+			{
+				m_enemies[i].transform.position = new Vector3(m_enemies[i].transform.position.x,
+				                                              m_enemies[i].transform.position.y - 1,
+				                                              m_enemies[i].transform.position.z);
+			}
+			//delete entities with position.y < 0
+
+			//make decisions for all other entities
+
+			//add new walls
+			int currentWallCount = 0;
+			float wallChance = 0.1f;
+			for (int i = 0; i < 9; ++i)
+			{
+				if (i == 0 || i == 8)
+				{
+					m_dungeon[i, 11] = TileTypes.WALL;
+					GameObject newWall = (GameObject)Instantiate (m_wall, new Vector3(i, 11, -1),transform.rotation);
+					m_walls.Add(newWall);
+				}
+				else if (currentWallCount < m_maxWallInLine)
+				{
+					if (Random.value < wallChance)
+					{
+						m_dungeon[i, 11] = TileTypes.WALL;
+						GameObject newWall = (GameObject)Instantiate (m_wall, new Vector3(i, 11, -1),transform.rotation);
+						m_walls.Add(newWall);
+
+						currentWallCount++;
+					}
+				}
+			}
+			//add new enemies
+
+			m_playerInput = false;
+		}
 	}
 }
